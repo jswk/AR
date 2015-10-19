b7725917bdb2557db76bb06d40b193efaa5e06d4
Jakub Sawicki <kuba@jswk.pl>
2015-10-19 10:40:25 +0200

    Changed output

--- lab1/zeus.sh
+++ lab1/zeus.sh
@@ -17,7 +17,7 @@ cp /people/plgjsawicki/AR/lab1/CA ./CA
 mkdir -p results
 
 for PROC in {1..12}; do
-    mpiexec -n $PROC ./CA 100 10 2> results/proc_$PROC
+    mpiexec -n $PROC ./CA 100 10 2&>1 | awk '{print "$PROC "$3}' >> results/proc_num
 done
 
 cp -r /tmp/$USER/AR/results /people/plgjsawicki/AR/lab1/results
