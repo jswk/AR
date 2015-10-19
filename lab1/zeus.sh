b2f161ed4f0db7f786d172581e9590a34bb32961
Jakub Sawicki <kuba@jswk.pl>
2015-10-19 10:42:23 +0200

    Output

--- lab1/zeus.sh
+++ lab1/zeus.sh
@@ -20,4 +20,4 @@ for PROC in {1..12}; do
     mpiexec -n $PROC ./CA 100 10 2&>1 | awk '{print "$PROC "$3}' >> results/proc_num
 done
 
-cp -r /tmp/$USER/AR/results /people/plgjsawicki/AR/lab1/results
+cp -r /tmp/$USER/AR/results /people/plgjsawicki/AR/lab1
