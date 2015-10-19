33a05ec10aba17a524d7c935c9baf0f580011025
Jakub Sawicki <kuba@jswk.pl>
2015-10-19 10:21:28 +0200

    Added zeus PBS script

---
+++ lab1/zeus.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#PBS -q plgrid-testing
+#PBS -l walltime=0:30:00
+#PBS -l nodes=1:ppn=12
+#PBS -A plgjsawicki2015b
+
+mkdir -p /tmp/$USER/AR
+cd /tmp/$USER/AR
+
+module load plgrid/tools/mpich/3.0.4
+
+cp /people/plgjsawicki/AR/lab1/AR ./AR
+mkdir results
+
+for PROC in {1..12}; do
+    mpiexec -n $PROC ./AR 100 10 > results/proc_$PROC
+done
