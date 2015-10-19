ea465eef20c0a36d7f959c3bd45402b74baceb25
Jakub Sawicki <kuba@jswk.pl>
2015-10-19 10:34:43 +0200

    Changed stream redirection, cleaning of directories

--- lab1/zeus.sh
+++ lab1/zeus.sh
@@ -4,14 +4,20 @@
 #PBS -l nodes=1:ppn=12
 #PBS -A plgjsawicki2015b
 
+rm -rf /tmp/$USER/AR
 mkdir -p /tmp/$USER/AR
-cd /tmp/$USER/AR
 
 module load plgrid/tools/mpich/3.0.4
 
+cd /people/plgjsawicki/AR/lab1
+make
+
+cd /tmp/$USER/AR
 cp /people/plgjsawicki/AR/lab1/CA ./CA
-mkdir results
+mkdir -p results
 
 for PROC in {1..12}; do
-    mpiexec -n $PROC ./CA 100 10 > results/proc_$PROC
+    mpiexec -n $PROC ./CA 100 10 2> results/proc_$PROC
 done
+
+cp -r /tmp/$USER/AR/results /people/plgjsawicki/AR/lab1/results
